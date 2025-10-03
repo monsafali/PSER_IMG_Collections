7143788910c70203b89f0d80ea7009bc5bb708df
monsafali <monsafali272@gmail.com>
2025-10-03 20:15:00 +0500

    frontend fixing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+export default defineConfig(({ mode }) => {
+  // Load env file based on the current mode (development or production)
+  const env = loadEnv(mode, process.cwd(), "");
 
-
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": import.meta.env.VITE_BACKEND_URL
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        "/api": {
+          target: env.VITE_BACKEND_URL, // ✅ use env variable here
+          changeOrigin: true,
+          secure: false,
+        },
+      },
     },
-  },
+  };
 });
