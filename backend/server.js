ee27bdbd49190590377335800a9eb3f584c7b23b
monsafali <monsafali272@gmail.com>
2025-10-03 18:53:48 +0500

    something new setting set

--- backend/server.js
+++ backend/server.js
@@ -35,12 +35,7 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+
 
 // app.use(fileUpload());
 
