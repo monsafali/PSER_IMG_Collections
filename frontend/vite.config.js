5789774c3af53a3dfa129b64f0ddfd98b3827a00
monsafali <monsafali272@gmail.com>
2025-10-03 20:28:20 +0500

    somethign again last change in frontend side

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig(({ mode }) => {
-  // Load env file based on the current mode (development or production)
-  const env = loadEnv(mode, process.cwd(), "");
 
-  return {
-    plugins: [react(), tailwindcss()],
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_BACKEND_URL, // ✅ use env variable here
-          changeOrigin: true,
-          secure: false,
-        },
-      },
+
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": "https://pser-img-collections.onrender.com"
     },
-  };
+  },
 });
+
+
