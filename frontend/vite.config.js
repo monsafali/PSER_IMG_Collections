11df92d1e6e64e42328946175a346b15e124301e
monsafali <monsafali272@gmail.com>
2025-10-04 07:03:21 +0500

    something aagain new chaning

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      "/api": "https://pser-img-collections.onrender.com"
+      "/api": "http://localhost:3000",
     },
   },
 });
