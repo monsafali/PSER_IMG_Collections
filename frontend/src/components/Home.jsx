cc9b89fd7a9fe628957e3909d9db4d19d408884c
monsafali <monsafali272@gmail.com>
2025-10-04 08:32:25 +0500

    somethign new added

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,10 +1,8 @@
 export default function Home() {
   return (
     <div className="p-6 text-center">
-      <h1 className="text-3xl font-bold mb-4">
-        Welcome to Testing Password App
-      </h1>
-      <p className="text-gray-700">This is the simple logiing using Mern .</p>
+      <h1 className="text-3xl font-bold mb-4">PSER IMAGE SAVER</h1>
+      <p className="text-gray-700">This is Simple Responded Image Saver app.</p>
     </div>
   );
 }
